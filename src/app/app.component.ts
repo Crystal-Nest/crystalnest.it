2fe21711533c4385c446a8713e116719ed071d96
Crystal Spider <riccardo.chimisso@gmail.com>
2024-02-04 16:29:34 +0100

    Eslint.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'cn-app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Crystal Nest';
+  public title = 'Crystal Nest';
 }
