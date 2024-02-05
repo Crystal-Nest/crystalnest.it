90fddefd38c7b2e8866b8f05704e98df16345b21
MoonstoneWebber <MoonstoneWebber>
2024-02-05 22:43:00 +0100

    Fix Multi-Loader Skeleton button typo and changed jszip to jszip-esm.

--- src/app/features/generator/generator.component.ts
+++ src/app/features/generator/generator.component.ts
@@ -1,27 +1,27 @@
 /* eslint-disable jsdoc/match-description */
 /* eslint-disable capitalized-comments */
-import {Component} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
-import JSZip from 'jszip';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import JSZip from '@progress/jszip-esm';
 
 @Component({
   selector: 'cn-generator',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './generator.component.html',
-  styleUrl: './generator.component.scss'
+  styleUrl: './generator.component.scss',
 })
 export class GeneratorComponent {
   /**
    * minecraft_version (select)
-   * 
+   *
    * group = crystalnest (input)
    * authors = Crystal Spider, Moonstone Webber (input)
    * mod_title = Cobweb Mod Template (input)
    * mod_id = mod_title, lowercase, replace blanks with _ (input)
    * mod_id_kebab = mod_id, replace _ with - (input)
    * github_user = crystal-nest (input)
-   * 
+   *
    * mod_version = 1.0.0.0 (input)
    * description = Multiloader template for Minecraft mods! (textarea)
    */
@@ -44,7 +44,7 @@ export class GeneratorComponent {
     mod?.file('LICENSE', '');
     mod?.file('README.md', '');
     mod?.file('settings.gradle', '');
-    zip.generateAsync({type: 'blob'}).then(content => {
+    zip.generateAsync({ type: 'blob' }).then((content) => {
       const anchor = document.createElement('a');
       anchor.style.display = 'none';
       anchor.href = URL.createObjectURL(content);
