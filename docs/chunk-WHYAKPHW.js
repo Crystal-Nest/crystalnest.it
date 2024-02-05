a85b15e7a406535b8212daad4d4487404901a09f
MoonstoneWebber <MoonstoneWebber>
2024-02-05 22:33:48 +0100

    Add template download button.

---
+++ docs/chunk-WHYAKPHW.js
@@ -0,0 +1,45 @@
+import{d as s,e as c,f as m,g as e,h as n,i,k as t,l as p,m as l,p as d,r as g}from"./chunk-ESQG6EZC.js";var f=()=>["/generator"],_=(()=>{let o=class o{};o.\u0275fac=function(r){return new(r||o)},o.\u0275cmp=s({type:o,selectors:[["cn-home"]],standalone:!0,features:[p],decls:65,vars:2,consts:[[3,"routerLink"],[1,"social"],["href","https://www.twitch.tv/crystal_spider_","target","_blank",1,"support"],["src","https://raw.githubusercontent.com/crystal-nest/mod-fancy-assets/main/twitch/twitch256.png"],["href","https://www.patreon.com/crystalspider","target","_blank",1,"support"],["src","https://raw.githubusercontent.com/crystal-nest/mod-fancy-assets/main/patreon/patreon256.png"],["href","https://ko-fi.com/crystalspider","target","_blank",1,"support"],["src","https://raw.githubusercontent.com/crystal-nest/mod-fancy-assets/main/kofi/kofi256.png"],["href","https://github.com/crystal-nest","target","_blank",1,"support"],["src","https://raw.githubusercontent.com/crystal-nest/mod-fancy-assets/main/github/github256.png"],["href","https://modrinth.com/user/CrystalSpider","target","_blank",1,"support"],["src","https://raw.githubusercontent.com/crystal-nest/mod-fancy-assets/main/modrinth/modrinth256.png"],["href","https://www.curseforge.com/members/crystal_spider_/projects","target","_blank",1,"support"],["src","https://raw.githubusercontent.com/crystal-nest/mod-fancy-assets/main/curseforge/curseforge256.png"],[1,"banner"],["href","https://bisecthosting.com/crystalspider","target","_blank",1,"support"],["src","https://www.bisecthosting.com/partners/custom-banners/d559b544-474c-4109-b861-1b2e6ca6026a.webp"]],template:function(r,u){r&1&&(e(0,"header"),t(1,`
+  `),e(2,"h1"),t(3,"Welcome to the Crystal Nest!"),n(),t(4,`
+`),n(),t(5,`
+
+`),e(6,"main"),t(7,`
+  `),e(8,"h2"),t(9,"We are dusting off some cobwebs, in the meantime..."),n(),t(10,`
+  `),e(11,"button",0),t(12,"Get your multy loader template here!"),n(),t(13,`
+  `),e(14,"div"),t(15,`
+    `),e(16,"h3"),t(17,"...consider supporting us!"),n(),t(18,`
+  `),n(),t(19,`
+`),n(),t(20,`
+
+`),e(21,"footer"),t(22,`
+  `),e(23,"div",1),t(24,`
+    `),e(25,"a",2),t(26,`
+      `),i(27,"img",3),t(28,`
+    `),n(),t(29,`
+
+    `),e(30,"a",4),t(31,`
+      `),i(32,"img",5),t(33,`
+    `),n(),t(34,`
+
+    `),e(35,"a",6),t(36,`
+      `),i(37,"img",7),t(38,`
+    `),n(),t(39,`
+
+    `),e(40,"a",8),t(41,`
+      `),i(42,"img",9),t(43,`
+    `),n(),t(44,`
+
+    `),e(45,"a",10),t(46,`
+      `),i(47,"img",11),t(48,`
+    `),n(),t(49,`
+
+    `),e(50,"a",12),t(51,`
+      `),i(52,"img",13),t(53,`
+    `),n(),t(54,`
+  `),n(),t(55,`
+  `),e(56,"div",14),t(57,`
+    `),e(58,"a",15),t(59,`
+      `),i(60,"img",16),t(61,`
+    `),n(),t(62,`
+  `),n(),t(63,`
+`),n(),t(64,`
+`)),r&2&&(c(11),m("routerLink",l(1,f)))},dependencies:[g,d],styles:["[_nghost-%COMP%]{display:flex;flex-direction:column;width:100%;height:100%;min-width:320px;padding:16px;background-color:#8f59b7;background-image:none,radial-gradient(circle,#ffffff4d,#82828298,#000000e6);background-repeat:no-repeat;background-size:cover;background-blend-mode:multiply;background-clip:border-box;text-align:center;font-family:Silkscreen;font-style:normal;color:#fefefe}header[_ngcontent-%COMP%]{display:flex;align-items:center;justify-content:center}header[_ngcontent-%COMP%]   h1[_ngcontent-%COMP%]{font-size:2.3em;font-weight:300}main[_ngcontent-%COMP%]{display:flex;flex-direction:column;justify-content:center;align-items:center;margin-top:32px;margin-bottom:auto}main[_ngcontent-%COMP%]   button[_ngcontent-%COMP%]{background-color:transparent;border:2px solid #fefefe;color:#fefefe;padding:12px 30px;cursor:pointer;font-size:1em;border-radius:8px;text-align:center;font-family:Silkscreen;font-style:normal;box-shadow:#32325d80 0 2px 5px -1px,#00000080 0 1px 3px -1px}main[_ngcontent-%COMP%]   button[_ngcontent-%COMP%]:hover{color:#8f59b7;background-color:#fefefe;box-shadow:#32325d 0 4px 10px -2px,#000 0 2px 6px -2px}main[_ngcontent-%COMP%]   h2[_ngcontent-%COMP%]{font-size:1.6em;font-weight:300;margin:.5em 0}main[_ngcontent-%COMP%]   div[_ngcontent-%COMP%]{display:flex;flex-direction:column;justify-content:center;align-items:center}main[_ngcontent-%COMP%]   div[_ngcontent-%COMP%]   h3[_ngcontent-%COMP%]{font-size:1.3em;font-weight:300;margin:.5em 0}footer[_ngcontent-%COMP%]   .social[_ngcontent-%COMP%]{display:flex;justify-content:center;gap:16px}footer[_ngcontent-%COMP%]   .social[_ngcontent-%COMP%]   .support[_ngcontent-%COMP%]{width:80px}footer[_ngcontent-%COMP%]   .banner[_ngcontent-%COMP%]{display:flex;justify-content:center;width:100%;margin-top:16px;border-radius:12px}.support[_ngcontent-%COMP%]{display:flex}.support[_ngcontent-%COMP%]   img[_ngcontent-%COMP%]{width:100%;border-radius:12px;box-shadow:#32325d80 0 2px 5px -1px,#00000080 0 1px 3px -1px;transition-duration:.2s}.support[_ngcontent-%COMP%]   img[_ngcontent-%COMP%]:hover{box-shadow:#32325d 0 4px 10px -2px,#000 0 2px 6px -2px;transform:translateY(-.15em)}"]});let a=o;return a})();export{_ as HomeComponent};
