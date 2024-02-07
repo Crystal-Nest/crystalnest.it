da25be2c1c2c419e8cb46837059560b6ead7839c
Crystal Spider <riccardo.chimisso@gmail.com>
2024-02-07 23:34:41 +0100

    Test to use the official GitHub API.

--- src/app/feature/generator/service/template.service.ts
+++ src/app/feature/generator/service/template.service.ts
@@ -28,14 +28,16 @@ export class TemplateService {
    * @returns {Observable<ArrayBuffer>}
    */
   public getTemplate(minecraftVersion: MinecraftVersion) {
-    // Return this.http.get(
-    //   `https://api.github.com/repos/${TEMPLATE_GITHUB_USER}/${TEMPLATE_MOD_ID_KEBAB}/zipball/${minecraftVersion}`,
-    //   {
-    //     // eslint-disable-next-line @typescript-eslint/naming-convention
-    //     Headers: {'X-GitHub-Api-Version': '2022-11-28'},
-    //     ResponseType: 'arraybuffer'
-    //   }
-    // );
-    return this.http.get(`https://github.com/${TEMPLATE_GITHUB_USER}/${TEMPLATE_MOD_ID_KEBAB}/archive/refs/heads/${minecraftVersion}.zip`, {responseType: 'arraybuffer'});
+    return this.http.get(
+      `https://api.github.com/repos/${TEMPLATE_GITHUB_USER}/${TEMPLATE_MOD_ID_KEBAB}/zipball/${minecraftVersion}`,
+      {
+        headers: {
+          // eslint-disable-next-line @typescript-eslint/naming-convention
+          'X-GitHub-Api-Version': '2022-11-28'
+        },
+        responseType: 'arraybuffer'
+      }
+    );
+    // Return this.http.get(`https://github.com/${TEMPLATE_GITHUB_USER}/${TEMPLATE_MOD_ID_KEBAB}/archive/refs/heads/${minecraftVersion}.zip`, {responseType: 'arraybuffer'});
   }
 }
