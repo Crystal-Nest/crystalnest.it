aa9fe558270ea352b529ef7667b98ed61aacd3af
Crystal Spider <riccardo.chimisso@gmail.com>
2024-02-23 01:31:10 +0100

    Update available Minecraft versions.

--- src/app/feature/generator/model/minecraft-version.type.ts
+++ src/app/feature/generator/model/minecraft-version.type.ts
@@ -4,4 +4,4 @@
  * @export
  * @typedef {MinecraftVersion}
  */
-export type MinecraftVersion = '1.20.2' | '1.20.4';
+export type MinecraftVersion = '1.20.4' | '1.20.2' | '1.19.4' | '1.19.2' | '1.18.2';
