1e459f4133a0f290f999671da603937dfdd3bbbd
Crystal Spider <riccardo.chimisso@gmail.com>
2024-03-03 18:36:40 +0100

    Add Noir to authors and improve skeleton generation.

--- src/app/feature/generator/generator.component.ts
+++ src/app/feature/generator/generator.component.ts
@@ -40,74 +40,139 @@ export class GeneratorComponent {
    * Builds the mod skeleton with the specified properties.
    *
    * @public
-   * @param {SkeletonForm} event
-   * @param {SkeletonForm} event.minecraftVersion
-   * @param {SkeletonForm} event.group
-   * @param {SkeletonForm} event.authors
-   * @param {SkeletonForm} event.modTitle
-   * @param {SkeletonForm} event.modId
-   * @param {SkeletonForm} event.modIdKebab
-   * @param {SkeletonForm} event.githubUser
-   * @param {SkeletonForm} event.description
-   * @param {SkeletonForm} event.crystalNestMod
+   * @param {SkeletonForm} skeletonForm
    */
-  public buildSkeleton({minecraftVersion, group, authors, modTitle, modId, modIdKebab, githubUser, description, crystalNestMod}: SkeletonForm) {
-    this.templateService.getTemplate(minecraftVersion).subscribe(templateZip => {
-      const zip = new JSZip();
-      new JSZip().loadAsync(templateZip).then(template => {
-        const root = `${TEMPLATE_MOD_ID_KEBAB}-${minecraftVersion}`;
-        template.forEach((path, entry) => {
-          switch (true) {
-            case entry.dir:
-              // Directory: replace the name of the root dir, group, and modid.
-              zip.folder(path.replace(root, modIdKebab).replaceAll(TEMPLATE_GROUP, group).replaceAll(TEMPLATE_MOD_ID, modId));
-              break;
-            case path.endsWith('gradle.properties'):
-              // File gradle.properties: replace all Gradle properties.
-              zip.file(
-                path.replace(root, modIdKebab),
-                entry.async('string').then(content => content
-                  .replace(TEMPLATE_GROUP, group)
-                  .replace(TEMPLATE_AUTHORS.join(', '), authors)
-                  .replace(TEMPLATE_MOD_TITLE, modTitle)
-                  .replace(TEMPLATE_MOD_ID_KEBAB, modIdKebab)
-                  .replace(TEMPLATE_MOD_ID, modId)
-                  .replace(/^description = .*$/m, `description = ${description.trim().replaceAll('\n', '\\n')}`)
-                  .replace(TEMPLATE_GITHUB_USER, githubUser))
-              );
-              break;
-            case path.endsWith('README.md'):
-              // File README.md: replace link references.
-              zip.file(
-                path.replace(root, modIdKebab),
-                entry.async('string').then(content => content
-                  .replace(TEMPLATE_BANNER_LINK, crystalNestMod ? `https://raw.githubusercontent.com/${group}/mod-fancy-assets/main/${modId}/banner.png` : 'Banner link here...')
-                  .replaceAll(`github.com/${TEMPLATE_GROUP}`, `github.com/${group}`)
-                  .replaceAll(TEMPLATE_MOD_TITLE, modTitle)
-                  .replaceAll(TEMPLATE_MOD_ID_KEBAB, modIdKebab)
-                  .replaceAll(TEMPLATE_MOD_ID, modId)
-                  .replace(TEMPLATE_SUPPORT_SECTION, `**Support us**\n\n${crystalNestMod ? content.split('**Support us**\n\n')[1] : 'Social links here...\n'}`))
-              );
-              break;
-            case !crystalNestMod && path.endsWith('build.gradle'):
-              // Handle build.gradle when it's not a Crystal Nest mod.
-              zip.file(
-                path.replace(root, modIdKebab),
-                entry.async('string').then(content => content.replace(/sonar(.|\n)*subprojects/, 'subprojects').replace(/plugins(.*\n){2}/, 'plugins {\n'))
-              );
-              break;
-            case crystalNestMod || !path.endsWith('.gitmodules'):
-              // Handle all files and, do not include Crystal Nest specific stuff.
-              zip.file(
-                path.replace(root, modIdKebab).replaceAll(TEMPLATE_GROUP, group).replaceAll(TEMPLATE_MOD_ID, modId),
-                entry.async('string').then(content => content.replaceAll(TEMPLATE_GROUP, group).replaceAll(TEMPLATE_MOD_ID_KEBAB, modIdKebab).replaceAll(TEMPLATE_MOD_ID, modId))
-              );
-              break;
-          }
-        });
-        zip.generateAsync({type: 'blob'}).then(file => this.download(file, modIdKebab));
-      });
+  public buildSkeleton(skeletonForm: SkeletonForm) {
+    this.templateService.getTemplate(skeletonForm.minecraftVersion).subscribe(
+      templateZip => new JSZip().loadAsync(templateZip).then(
+        template => this[`processTemplate${skeletonForm.crystalNestMod ? 'Nest' : 'Others'}`](template, skeletonForm).generateAsync({type: 'blob'}).then(file => this.download(file, skeletonForm.modIdKebab))
+      )
+    );
+  }
+
+  /**
+   * Processes the given zip template according to the provided properties.  
+   * Crystal Nest mods only.
+   *
+   * @private
+   * @param {JSZip} template
+   * @param {SkeletonForm} skeletonForm
+   * @param {SkeletonForm} skeletonForm.minecraftVersion
+   * @param {SkeletonForm} skeletonForm.modId
+   * @param {SkeletonForm} skeletonForm.modIdKebab
+   * @param {SkeletonForm} skeletonForm.modTitle
+   * @param {SkeletonForm} skeletonForm.description
+   * @returns {JSZip}
+   */
+  private processTemplateNest(template: JSZip, {minecraftVersion, modId, modIdKebab, modTitle, description}: SkeletonForm) {
+    const zip = new JSZip();
+    const root = `${TEMPLATE_MOD_ID_KEBAB}-${minecraftVersion}`;
+    template.forEach((path, entry) => {
+      switch (true) {
+        case entry.dir:
+          // Directory: replace the name of the root dir and modid.
+          zip.folder(path.replace(root, modIdKebab).replaceAll(TEMPLATE_MOD_ID, modId));
+          break;
+        case path.endsWith('gradle.properties'):
+          // File gradle.properties: replace Gradle properties.
+          zip.file(
+            path.replace(root, modIdKebab),
+            entry.async('string').then(content => content
+              .replace(TEMPLATE_MOD_TITLE, modTitle)
+              .replace(TEMPLATE_MOD_ID_KEBAB, modIdKebab)
+              .replace(TEMPLATE_MOD_ID, modId)
+              .replace(/^description = .*$/m, `description = ${description.trim().replaceAll('\n', '\\n')}`))
+          );
+          break;
+        case path.endsWith('README.md'):
+          // File README.md: replace link references.
+          zip.file(
+            path.replace(root, modIdKebab),
+            entry.async('string').then(content => content.replaceAll(TEMPLATE_MOD_TITLE, modTitle).replaceAll(TEMPLATE_MOD_ID_KEBAB, modIdKebab).replaceAll(TEMPLATE_MOD_ID, modId))
+          );
+          break;
+        default:
+          // Handle all files.
+          zip.file(
+            path.replace(root, modIdKebab).replaceAll(TEMPLATE_MOD_ID, modId),
+            entry.async('string').then(content => content.replaceAll(TEMPLATE_MOD_ID_KEBAB, modIdKebab).replaceAll(TEMPLATE_MOD_ID, modId))
+          );
+          break;
+      }
     });
+    return zip;
+  }
+
+  /**
+   * Processes the given zip template according to the provided properties.  
+   * Not Crystal Nest mods only.
+   *
+   * @private
+   * @param {JSZip} template
+   * @param {SkeletonForm} skeletonForm
+   * @param {SkeletonForm} skeletonForm.minecraftVersion
+   * @param {SkeletonForm} skeletonForm.group
+   * @param {SkeletonForm} skeletonForm.modId
+   * @param {SkeletonForm} skeletonForm.modIdKebab
+   * @param {SkeletonForm} skeletonForm.modTitle
+   * @param {SkeletonForm} skeletonForm.authors
+   * @param {SkeletonForm} skeletonForm.description
+   * @param {SkeletonForm} skeletonForm.githubUser
+   * @returns {JSZip}
+   */
+  private processTemplateOthers(template: JSZip, {minecraftVersion, group, modId, modIdKebab, modTitle, authors, description, githubUser}: SkeletonForm) {
+    const zip = new JSZip();
+    const root = `${TEMPLATE_MOD_ID_KEBAB}-${minecraftVersion}`;
+    template.forEach((path, entry) => {
+      switch (true) {
+        case entry.dir && !path.includes('.github'):
+          // Directory: replace the name of the root dir, group, and modid.
+          zip.folder(path.replace(root, modIdKebab).replaceAll(TEMPLATE_GROUP, group).replaceAll(TEMPLATE_MOD_ID, modId));
+          break;
+        case path === `${root}/build.gradle`:
+          // Handle build.gradle when it's not a Crystal Nest mod.
+          zip.file(
+            path.replace(root, modIdKebab),
+            entry.async('string').then(content => content.replace(/sonar {[^]+subprojects/, 'subprojects').replace(/plugins(.*\r?\n){2}/, 'plugins {\n'))
+          );
+          break;
+        case path.endsWith('gradle.properties'):
+          // File gradle.properties: replace Gradle properties.
+          zip.file(
+            path.replace(root, modIdKebab),
+            entry.async('string').then(content => content
+              .replace(TEMPLATE_GROUP, group)
+              .replace(TEMPLATE_AUTHORS.join(', '), authors)
+              .replace(TEMPLATE_MOD_TITLE, modTitle)
+              .replace(TEMPLATE_MOD_ID_KEBAB, modIdKebab)
+              .replace(TEMPLATE_MOD_ID, modId)
+              .replace(/^description = .*$/m, `description = ${description.trim().replaceAll('\n', '\\n')}`)
+              .replace(TEMPLATE_GITHUB_USER, githubUser))
+          );
+          break;
+        case path.endsWith('README.md'):
+          // File README.md: replace link references.
+          zip.file(
+            path.replace(root, modIdKebab),
+            entry.async('string').then(content => content
+              .replace(TEMPLATE_BANNER_LINK, 'Banner link here...')
+              .replaceAll(`github.com/${TEMPLATE_GROUP}`, `github.com/${group}`)
+              .replaceAll(TEMPLATE_MOD_TITLE, modTitle)
+              .replaceAll(TEMPLATE_MOD_ID_KEBAB, modIdKebab)
+              .replaceAll(TEMPLATE_MOD_ID, modId)
+              .replace(TEMPLATE_SUPPORT_SECTION, '**Support us**\n\nSocial links here...\n'))
+          );
+          break;
+        case !path.includes('.github'):
+          // Handle all files without including Crystal Nest specific stuff.
+          zip.file(
+            path.replace(root, modIdKebab).replaceAll(TEMPLATE_GROUP, group).replaceAll(TEMPLATE_MOD_ID, modId),
+            entry.async('string').then(content => content.replaceAll(TEMPLATE_GROUP, group).replaceAll(TEMPLATE_MOD_ID_KEBAB, modIdKebab).replaceAll(TEMPLATE_MOD_ID, modId))
+          );
+          break;
+      }
+    });
+    return zip;
   }
 
   /**
@@ -115,13 +180,13 @@ export class GeneratorComponent {
    *
    * @private
    * @param {Blob} file
-   * @param {string} modId
+   * @param {string} id
    */
-  private download(file: Blob, modId: string) {
+  private download(file: Blob, id: string) {
     const anchor = document.createElement('a');
     anchor.style.display = 'none';
     anchor.href = URL.createObjectURL(file);
-    anchor.download = `cobweb-mod-skeleton (${modId})`;
+    anchor.download = `cobweb-mod-skeleton (${id})`;
     document.body.appendChild(anchor);
     anchor.click();
     anchor.remove();
--- src/app/feature/generator/model/template.constants.ts
+++ src/app/feature/generator/model/template.constants.ts
@@ -1,5 +1,5 @@
 export const TEMPLATE_GROUP = 'crystalnest';
-export const TEMPLATE_AUTHORS = ['Crystal Spider', 'Moonstone Webber'];
+export const TEMPLATE_AUTHORS = ['Crystal Spider', 'Moonstone Webber', 'Noir'];
 export const TEMPLATE_MOD_ID = 'cobweb_mod_template';
 export const TEMPLATE_MOD_ID_KEBAB = 'cobweb-mod-template';
 export const TEMPLATE_MOD_TITLE = 'Cobweb Mod Template';
