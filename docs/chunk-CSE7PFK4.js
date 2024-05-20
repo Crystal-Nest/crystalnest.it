cfbc3ac5db1dc2b9174ca8a7420bdd1ec3d9807b
Crystal Spider <riccardo.chimisso@gmail.com>
2024-05-21 01:10:59 +0200

    Build update.

--- docs/chunk-CSE7PFK4.js
+++ docs/chunk-CSE7PFK4.js
@@ -1,4 +1,4 @@
-import{f as m}from"./chunk-5XIRWGHL.js";import"./chunk-PA6SZ2RC.js";import{$b as e,Jb as n,Kb as i,fc as s,sa as r}from"./chunk-CHL5XQK7.js";var h=(()=>{let t=class t{};t.\u0275fac=function(l){return new(l||t)},t.\u0275cmp=r({type:t,selectors:[["cn-home"]],standalone:!0,features:[s],decls:12,vars:0,template:function(l,c){l&1&&(n(0,"main"),e(1,`
+import{f as m}from"./chunk-3UENRRUO.js";import"./chunk-DYXA464R.js";import{$b as e,Jb as n,Kb as i,fc as s,sa as r}from"./chunk-TFCPD5PN.js";var h=(()=>{let t=class t{};t.\u0275fac=function(l){return new(l||t)},t.\u0275cmp=r({type:t,selectors:[["cn-home"]],standalone:!0,features:[s],decls:12,vars:0,template:function(l,c){l&1&&(n(0,"main"),e(1,`
   `),n(2,"h1"),e(3,"Welcome to the Crystal Nest!"),i(),e(4,`
   `),n(5,"h1"),e(6,"We are dusting off some cobwebs, in the meantime..."),i(),e(7,`
   `),n(8,"h1"),e(9,"...consider supporting us!"),i(),e(10,`
--- src/app/feature/frame/component/loader/loader.component.ts
+++ src/app/feature/frame/component/loader/loader.component.ts
@@ -16,12 +16,30 @@ import {MatProgressBarModule, ProgressBarMode} from '@angular/material/progress-
   styleUrl: './loader.component.scss'
 })
 export class LoaderComponent {
+  /**
+   * Whether there is any loading.
+   *
+   * @public
+   * @type {!(boolean | null)}
+   */
   @Input({required: true})
   public loading!: boolean | null;
 
+  /**
+   * Loading bar mode.
+   *
+   * @public
+   * @type {ProgressBarMode}
+   */
   @Input({transform: (value: ProgressBarMode | null) => value ?? 'indeterminate'})
   public progressMode: ProgressBarMode = 'indeterminate';
 
+  /**
+   * Optional loading progress.
+   *
+   * @public
+   * @type {number}
+   */
   @Input({transform: (value: number | null) => value ?? -1})
   public progress = -1;
 }
